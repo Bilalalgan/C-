a85533d283e05cd99903a2b24a18c6e41acbe433
Bilal Çağrı Alğan <134233797+Bilalalgan@users.noreply.github.com>
2024-02-19 14:12:12 +0300

    Add files via upload
    C#-6-Collections

---
+++ C#-6-Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Array
+           int[] sayilar = new int[5];
+           sayilar[0] = 10;
+
+           //Collections
+            //Non-generic C.
+                //** ArrayList,SortedList
+            //Generic list <str>
+                //** int,string,product
+        }
+    }
+}
