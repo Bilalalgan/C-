a9c65cac6fd48e4f612e25f1674e68242beb8118
Bilal Çağrı Alğan <134233797+Bilalalgan@users.noreply.github.com>
2024-02-25 13:48:13 +0300

    Add files via upload
    C#-7-Hata Yönetimi

---
+++ C#-7-Hata Yönetimi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("a: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("b: ");
+                int b = int.Parse(Console.ReadLine());  
+
+                var sonuc = a / b;
+
+                Console.WriteLine("{0} / {1} = {2}",a,b,sonuc);
+        }
+    }
+}
